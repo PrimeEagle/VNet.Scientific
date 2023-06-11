84a2b9d212fe360366c7773a845f5da0ca1be680
PrimeEagle <misc@hiddendragon.com>
2023-06-11 13:58:43 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.Scientific@1149 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

--- VNet.Scientific.CodeGen/DimensionSourceGenerator.cs
+++ VNet.Scientific.CodeGen/DimensionSourceGenerator.cs
@@ -1,130 +1,125 @@
-﻿//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using Microsoft.CodeAnalysis;
-//using Microsoft.CodeAnalysis.CSharp;
-//using Microsoft.CodeAnalysis.CSharp.Syntax;
-//using Microsoft.CodeAnalysis.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using VNet.CodeGeneration.Writers.CodeWriter;
+using VNet.CodeGeneration.Writers.CodeWriter.Languages.CSharp;
 
-//namespace VNet.Scientific.CodeGen
-//{
-//    [Generator]
-//    public class DimensionSourceGenerator : ISourceGenerator
-//    {
-//        public void Initialize(GeneratorInitializationContext context)
-//        {
+namespace VNet.Scientific.CodeGen
+{
+    [Generator]
+    public class DimensionSourceGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+
+
+            context.RegisterForSyntaxNotifications(() => new TestSyntaxReceiver());
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
 //#if DEBUG
 //            if (!System.Diagnostics.Debugger.IsAttached)
 //            {
 //                System.Diagnostics.Debugger.Launch();
 //            }
 //#endif
+            var cSharp = new CSharpLanguageSettings(new CSharpDefaultStyle());
 
-//                var receiver = new MySyntaxReceiver();
-//                context.RegisterForSyntaxNotifications(() => receiver);
-
-//        }
-
-//        public void Execute(GeneratorExecutionContext context)
-//        {
-//            var compilation = context.Compilation;
-//            var syntaxTrees = compilation.SyntaxTrees;
-
-//            foreach (var syntaxTree in syntaxTrees)
-//            {
-//                if (syntaxTree.FilePath.EndsWith("UnitDefinitionComponents.cs"))
-//                {
-//                    var root = syntaxTree.GetRoot();
-//                    var semanticModel = compilation.GetSemanticModel(syntaxTree);
-
-//                    var fieldDeclarations = root.DescendantNodes().OfType<FieldDeclarationSyntax>();
-//                    foreach (var fieldDeclaration in fieldDeclarations)
-//                    {
-//                        foreach (var variable in fieldDeclaration.Declaration.Variables)
-//                        {
-//                            var fieldSymbol = semanticModel.GetDeclaredSymbol(variable) as IFieldSymbol;
-//                            if (fieldSymbol?.Name == "Components" && fieldSymbol.Type is INamedTypeSymbol namedTypeSymbol)
-//                            {
-//                                if (namedTypeSymbol.Arity == 2 && namedTypeSymbol.Name == "Dictionary")
-//                                {
-//                                    var initializer = fieldDeclaration.DescendantNodes().OfType<EqualsValueClauseSyntax>().FirstOrDefault();
-//                                    if (initializer != null && initializer.Value is ObjectCreationExpressionSyntax objectCreation && objectCreation.Initializer != null)
-//                                    {
-//                                        var dictionaryEntries = objectCreation.Initializer.Expressions.OfType<AssignmentExpressionSyntax>().ToList();
-//                                        if (dictionaryEntries != null)
-//                                        {
-//                                            foreach (var entry in dictionaryEntries)
-//                                            {
-//                                                var identifier = entry.Left.ToString();
-//                                                var objectCreationSyntax = entry.Right as ObjectCreationExpressionSyntax;
-
-//                                                // Process the dictionary entry as needed
-//                                            }
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
+            var code = CodeWriter.CreateCodeFile()
+                                        .UsingLanguageSettings(cSharp)
+                                            .AddNamespace("test.name.space").WithScopedStyle()
+                                                .AddComment("this is a comment").ThatIsSingleLine()
+                                                .AddClass("class")
+                                                    .AddMethod("method")
+                                                    .Up()
+                                                .Up()
+                                            .Up()
+                                        .Generate();
 
 
 
+            if (!(context.SyntaxContextReceiver is TestSyntaxReceiver receiver))
+            {
+                return;
+            }
+
+            foreach (var dimension in receiver.Dimensions)
+            {
+                if (dimension == "Scalar") continue;
+
+                var fileName = dimension + ".g.cs";
+
+                try
+                {
+                    context.AddSource(fileName, SourceText.From(code, Encoding.UTF8));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
 
 
+        public class TestSyntaxReceiver : ISyntaxContextReceiver
+        {
+            public readonly List<string> Dimensions = new List<string>();
 
+            public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+            {
+                try
+                {
+                    if (!(context.Node is ClassDeclarationSyntax classDeclarationSyntax) || !classDeclarationSyntax.Modifiers.Any(SyntaxKind.PartialKeyword) || classDeclarationSyntax.Identifier.Text != "UnitDefinition") return;
+                    // Get all of the FieldDeclarationSyntax nodes
+                    var fieldDeclarations = classDeclarationSyntax.DescendantNodes().OfType<FieldDeclarationSyntax>();
 
+                    // Filter the fields to only include public static fields
+                    var publicStaticFields = fieldDeclarations.Where(f =>
+                        f.Modifiers.Any(m => m.ValueText == "public") &&
+                        f.Modifiers.Any(m => m.ValueText == "static"));
 
+                    // Loop through the public static fields
+                    foreach (var field in publicStaticFields)
+                    {
+                        // Get the type of the field
+                        var fieldType = field.Declaration.Type;
 
+                        // Check if the field is a Dictionary
+                        if (!(fieldType is GenericNameSyntax genericNameSyntax) ||
+                            genericNameSyntax.Identifier.Text != "Dictionary") continue;
+                        // Loop through the variables (fields) in the declaration.
+                        foreach (var variable in field.Declaration.Variables)
+                        {
+                            var fieldName = variable.Identifier.Text;
 
+                            if (fieldName != "Components" || variable.Initializer == null) continue;
+                            if (!(variable.Initializer.Value is ImplicitObjectCreationExpressionSyntax objCreation)
+                                || !(objCreation.Initializer is InitializerExpressionSyntax initializer)) continue;
+                            foreach (var expression in initializer.Expressions)
+                            {
+                                if (!(expression is InitializerExpressionSyntax dictInitializer)) continue;
+                                if (!(dictInitializer.Expressions[0] is LiteralExpressionSyntax key)) continue;
 
-
-
-
-//        private string GenerateCode(string typeName, List<ITypeSymbol> tupleTypeArguments)
-//        {
-//            // Generate code based on the literal and tuple expressions
-//            // Use 'typeName' and 'tupleTypeArguments' to generate the desired code
-//            // Return the generated code as a string
-
-//            var unitType = typeName;
-
-//            string source = @$"
-//using System.Numerics;
-
-//namespace VNet.Scientific.Measurement
-//{{
-//    public sealed class {unitType}<TVal> : DimensionBase<{unitType}Unit, TVal> where TVal : notnull, INumber<TVal>
-//    {{
-//        public override string IdTag => nameof({unitType}<TVal>);
-
-//        public {unitType}() : base()
-//        {{
-//            Initialize(IdTag);
-//        }}
-//    }}
-//}}";
-
-//            return source;
-//        }
-//    }
-
-//    internal class MySyntaxReceiver : ISyntaxReceiver
-//    {
-//        public List<AssignmentExpressionSyntax> DictionaryEntries { get; } = new List<AssignmentExpressionSyntax>();
-
-//        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
-//        {
-//            if (syntaxNode is AssignmentExpressionSyntax assignmentExpression &&
-//                assignmentExpression.Left is LiteralExpressionSyntax literalExpression &&
-//                assignmentExpression.Right is ObjectCreationExpressionSyntax objectCreationExpression &&
-//                objectCreationExpression.Initializer != null)
-//            {
-//                DictionaryEntries.Add(assignmentExpression);
-//            }
-//        }
-//    }
-
-//}
+                                var keyValue = key.Token.Value?.ToString();
+                                lock (Dimensions)
+                                {
+                                    Dimensions.Add(keyValue);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
