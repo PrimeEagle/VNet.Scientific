7c7b11046169639c76cc5b604129c63d70dc6559
PrimeEagle <misc@hiddendragon.com>
2023-09-30 23:40:32 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.Scientific@1293 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

--- VNet.Scientific.CodeGen/DimensionSourceGenerator.cs
+++ VNet.Scientific.CodeGen/DimensionSourceGenerator.cs
@@ -35,7 +35,7 @@ namespace VNet.Scientific.CodeGen
             try
             {
                 log.Initialize(@"D:\generator.log");
-               // log.Clear();
+                log.Clear();
                 var fileNameMapping = Path.Combine(context.ProjectDir(), "Measurement", vNetJsonDir, "_unitNetToVNetMappings.json");
                 string mappingJson;
 
@@ -44,15 +44,21 @@ namespace VNet.Scientific.CodeGen
                     mappingJson = reader.ReadToEnd();
                 }
 
+                log.WriteLine("1");
                 var mapping = Json.Deserialize<List<UnitNetVNetMappingEntry>>(mappingJson.Trim());
+                log.WriteLine("2");
 
                 var fileNameUnitNet = Path.Combine(context.ProjectDir(), "Measurement", unitNetJsonDir, "_dimensionHashFile.json");
+                log.WriteLine("3");
                 var dimensionHashFileUnitNet = new FileComparer(fileNameUnitNet);
+                log.WriteLine("4");
                 dimensionHashFileUnitNet.Update();
+                log.WriteLine("5");
                 dimensionHashFileUnitNet.Save();
-
+                log.WriteLine("6");
                 foreach (var unitNetFile in dimensionHashFileUnitNet.GetUpdatedEntries())
                 {
+                    log.WriteLine(unitNetFile.FileName);
                     var dimUnitNet = Json.Deserialize<UnitNetDimension>(unitNetFile.GetJson());
                     log.WriteLine($"converting {dimUnitNet.Name}");
                     var dimVNet = VNetDimension.ConvertFrom(dimUnitNet, mapping);
@@ -65,6 +71,7 @@ namespace VNet.Scientific.CodeGen
                 var dimensionHashFileVNet = new FileComparer(fileNameVNet);
                 dimensionHashFileVNet.Update();
                 dimensionHashFileVNet.Save();
+                log.WriteLine("wrote _dimensionHashFile.json");
 
                 foreach (var dimVNetFile in dimensionHashFileVNet.GetUpdatedEntries())
                 {
@@ -145,10 +152,10 @@ namespace VNet.Scientific.CodeGen
                     log.WriteLine("done");
                 }
                 var dimensionDefFileName = Path.Combine(context.ProjectDir(), "Measurement", "DimensionDef");
-
+                log.WriteLine("wrote " + dimensionDefFileName);
 
                 var exponentsDictLines = new List<string>();
-
+                
                 var targetFolder = Path.Combine(context.ProjectDir(), "Measurement", "VNet.Json");
                 var genFiles = Directory.GetFiles(targetFolder);
                 exponentsDictLines.Add("public static Dictionary<string, DimensionExponents> Exponents = new Dictionary<string, DimensionExponents>()");
@@ -176,108 +183,6 @@ namespace VNet.Scientific.CodeGen
                                     .UpTo<NamespaceScope>()
                                 .UpTo<CSharpCodeFile>()
                               .Save(dimensionDefFileName);
-                //var targetPath = Path.Combine(context.ProjectDir(), "Measurement", "Dimensions");
-                //var di = new DirectoryInfo(targetPath);
-
-                //foreach (FileInfo file in di.GetFiles())
-                //{
-                //    file.Delete();
-                //}
-
-                // code gen VNet JSON files
-                //var fileNameVNet = Path.Combine(context.ProjectDir(), "Measurement", vNetJsonDir, "_dimensionHashFile.json");
-                //var dimensionHashFileVNet = new FileComparer(fileNameVNet);
-                //dimensionHashFileVNet.Update();
-                //dimensionHashFileVNet.Save();
-
-                //            foreach (var vNetFile in dimensionHashFileVNet.GetUpdatedEntries())
-                //            {
-                //                var dimVNet = Json.Deserialize<VNetDimension>(vNetFile.GetJson());
-
-                //                if (dimVNet.Name == "Density")
-                //                {
-                //                    log.WriteLine("before");
-                //                    foreach (var key in dimVNet.ConversionFunctions.Keys)
-                //                    {
-                //                        log.WriteLine($"     {dimVNet.ConversionFunctions[key]}");
-                //                    }
-                //                }
-
-                //                VNetDimension.ConversionPostProcess(dimVNet);
-
-                //                if (dimVNet.Name == "Density")
-                //                {
-                //                    log.WriteLine("after");
-                //                    foreach (var key in dimVNet.ConversionFunctions.Keys)
-                //                    {
-                //                        log.WriteLine($"     {dimVNet.ConversionFunctions[key]}");
-                //                    }
-                //                }
-
-                //                var saveFileName = Path.Combine(context.ProjectDir(), "Measurement", vNetJsonDir, dimVNet.Name + ".json");
-                //                dimVNet.Save(saveFileName);
-
-                //                var targetFileName = Path.Combine(context.ProjectDir(), "Measurement", "Dimensions", dimVNet.Name + "Unit");
-                //                if(File.Exists($"{targetFileName}.g.cs")) File.Delete($"{targetFileName}.g.cs");
-
-                //                //log.WriteLine($"generating unit class for {dimVNet.Name}");
-                //                CodeWriter.For<CSharpCodeFile>()
-                //                    .AddComment($"Auto-generated for VNet on {DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")}")
-                //                    .AddBlankLines(2)
-                //                    .AddScopedNamespace("VNet.Scientific.Measurement.Dimensions")
-                //                        .AddEnum($"{dimVNet.Name}Unit")
-                //                            .WithModifier("public")
-                //                            .AddMembers(dimVNet.Units)
-                //                            .Sort()
-                //                        .UpTo<NamespaceScope>()
-                //                    .UpTo<CSharpCodeFile>()
-                //                .Save(targetFileName);
-
-                //                targetFileName = Path.Combine(context.ProjectDir(), "Measurement", "Dimensions", dimVNet.Name);
-                //                if (File.Exists($"{targetFileName}.g.cs")) File.Delete($"{targetFileName}.g.cs");
-
-                //                var dictLines = new List<string>();
-                //                foreach(var key in dimVNet.ConversionFunctions.Keys)
-                //                {
-                //;                   //log.WriteLine($"     {dimVNet.ConversionFunctions[key]}");
-                //                    dictLines.Add($"ConversionFunctions.Add({dimVNet.Name}Unit.{key}, \"{dimVNet.ConversionFunctions[key]}\");");
-                //                }
-
-                //                CodeWriter.For<CSharpCodeFile>()
-                //                          .AddComment($"Auto-generated for VNet on {DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")}")
-                //                          .AddUsing("System.Numerics")
-                //                          .AddBlankLines(2)
-                //                          .AddScopedNamespace("VNet.Scientific.Measurement.Dimensions")
-                //                            .AddClass(dimVNet.Name)
-                //                                .WithModifier("public")
-                //                                .WithModifier("sealed")
-                //                                .DerivedFrom($"DimensionBase<{dimVNet.Name}Unit, TVal>")
-                //                                .WithGenericType("TVal")
-                //                                .WithGenericConstraint("TVal : notnull, INumber<TVal>")
-                //                                .AddCodeLine($"public override string IdTag => nameof({dimVNet.Name}<TVal>);")
-                //                                .AddBlankLine()
-                //                                .AddMethod()
-                //                                    .ThatIsAConstructor()
-                //                                    .WithModifier("public")
-                //                                    .AddCodeLine($"DimensionComponent.Exponents.Length = {dimVNet.Exponents[0]};")
-                //                                    .AddCodeLine($"DimensionComponent.Exponents.Mass = {dimVNet.Exponents[1]};")
-                //                                    .AddCodeLine($"DimensionComponent.Exponents.Time = {dimVNet.Exponents[2]};")
-                //                                    .AddCodeLine($"DimensionComponent.Exponents.ElectricalCurrent = {dimVNet.Exponents[3]};")
-                //                                    .AddCodeLine($"DimensionComponent.Exponents.LuminousIntensity = {dimVNet.Exponents[4]};")
-                //                                    .AddCodeLine($"DimensionComponent.Exponents.Temperature = {dimVNet.Exponents[5]};")
-                //                                    .AddCodeLine($"DimensionComponent.Exponents.Amount = {dimVNet.Exponents[6]};")
-                //                                    .AddBlankLine()
-                //                                    .AddCodeLine($"DefaultUnit = {dimVNet.Name}Unit.{dimVNet.DefaultUnit};")
-                //                                    .AddBlankLine()
-                //                                    .AddCodeLine($"")
-                //                                    .AddCodeLines(dictLines)
-                //                                    .UpTo<ClassScope>()
-                //                                .UpTo<NamespaceScope>()
-                //                            .UpTo<CSharpCodeFile>()
-                //                          .Save(targetFileName);
-
-                //                log.WriteLine("done");
-                //            }
             }
             catch (Exception ex)
             {
