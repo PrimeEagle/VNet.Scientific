daa40f52b4e6ae19d533c5d5551e7e7b5cd07620
PrimeEagle <misc@hiddendragon.com>
2023-10-09 18:34:35 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.Scientific@1353 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

---
+++ VNet.Scientific/NumericalVolumes/VolumeFunctions.cs
@@ -0,0 +1,33 @@
+﻿namespace VNet.Scientific.NumericalVolumes
+{
+    public static class VolumeFunctions
+    {
+        public static void RunVolumeFunction(int x, int y, int z, Action<int, int, int> function)
+        {
+            for (var i = 0; i < x; i++)
+            {
+                for (var j = 0; j < y; j++)
+                {
+                    for (var k = 0; k < z; k++)
+                    {
+                        function(i, j, k);
+                    }
+                }
+            }
+        }
+
+        public static void RunNeighborFunction(int lowerBound, int upperBound, Action<int, int, int> function)
+        {
+            for (var dx = lowerBound; dx <= upperBound; dx++)
+            {
+                for (var dy = lowerBound; dy <= upperBound; dy++)
+                {
+                    for (var dz = lowerBound; dz <= upperBound; dz++)
+                    {
+                        function(dx, dy, dz);
+                    }
+                }
+            }
+        }
+    }
+}
